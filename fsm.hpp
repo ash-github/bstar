409b3ee680e6ac0aab390a01bb710c3756bd1122
Gogs <gogs@fake.local>
2016-10-12 19:46:09 +0800

    add

---
+++ fsm.hpp
@@ -0,0 +1,128 @@
+#pragma once
+#include <iostream>
+#include <cassert>
+#include <chrono>
+
+enum State
+{
+	STATE_PRIMARY,          //  Primary, waiting for peer to connect
+	STATE_BACKUP,           //  Backup, waiting for peer to connect
+	STATE_ACTIVE,           //  Active - accepting connections
+	STATE_PASSIVE           //  Passive - not accepting connections
+};
+
+//  Events, which start with the states our peer can be in
+enum Event
+{
+	PEER_PRIMARY,           //  HA peer is pending primary
+	PEER_BACKUP,            //  HA peer is pending backup
+	PEER_ACTIVE,            //  HA peer is active
+	PEER_PASSIVE,           //  HA peer is passive
+	CLIENT_REQUEST          //  Client makes request
+};
+
+struct fsm_t
+{
+	//  Our finite state machine
+	State state;        //  Current state
+	Event event;        //  Current event
+	int64_t peerExpiry;    //  When peer is considered 'dead'
+
+							   //  We send state information this often
+							   //  If peer doesn't respond in two heartbeats, it is 'dead'
+	const std::int64_t HEARTBEAT = 1000;          //  In msecs
+											   //  The heart of the Binary Star design is its finite-state machine (FSM).
+											   //  The FSM runs one event at a time. We apply an event to the current state,
+											   //  which checks if the event is accepted, and if so, sets a new state:
+public:
+
+	bool state_machine()
+	{
+		bool exception = false;
+
+		//  These are the PRIMARY and BACKUP states; we're waiting to become
+		//  ACTIVE or PASSIVE depending on events we get from our peer:
+		if (state == State::STATE_PRIMARY)
+		{
+			if (event == Event::PEER_BACKUP)
+			{
+				std::cout << "I: connected to backup (passive), ready active\n";
+				state = State::STATE_ACTIVE;
+				std::cout << "I: current state STATE_ACTIVE\n";
+			}
+			else if (event == Event::PEER_ACTIVE)
+			{
+				std::cout << "I: connected to backup (active), ready passive\n";
+				state = State::STATE_PASSIVE;
+				std::cout << "I: current state STATE_PASSIVE\n";
+			}
+			//  Accept client connections
+		}
+		else if (state == State::STATE_BACKUP)
+		{
+			if (event == Event::PEER_ACTIVE)
+			{
+				std::cout << "I: connected to primary (active), ready passive\n";
+				state = State::STATE_PASSIVE;
+				std::cout << "I: current state STATE_PASSIVE\n";
+			}
+			else if (event == Event::CLIENT_REQUEST)//  Reject client connections when acting as backup
+				exception = true;
+		}
+		else if (state == State::STATE_ACTIVE) //These are the ACTIVE and PASSIVE states:
+		{
+			if (event == Event::PEER_ACTIVE)
+			{
+				//  Two actives would mean split-brain
+				std::cout << "E: fatal error - dual actives, aborting\n";
+				exception = true;
+			}
+		}
+		else if (state == State::STATE_PASSIVE) //  Server is passive //  CLIENT_REQUEST events can trigger failover if peer looks dead		
+		{
+			if (event == Event::PEER_PRIMARY)
+			{
+				//  Peer is restarting - become active, peer will go passive
+				std::cout << "I: primary (passive) is restarting, ready active\n";
+				state = State::STATE_ACTIVE;
+			}
+			else if (event == Event::PEER_BACKUP)
+			{
+				//  Peer is restarting - become active, peer will go passive
+				std::cout << "I: backup (passive) is restarting, ready active\n";
+				state = State::STATE_ACTIVE;
+			}
+			else if (event == Event::PEER_PASSIVE)
+			{
+				//  Two passives would mean cluster would be non-responsive
+				std::cout << "E: fatal error - dual passives, aborting\n";
+				exception = true;
+			}
+			else if (event == Event::CLIENT_REQUEST)
+			{
+				//  Peer becomes active if timeout has passed
+				//  It's the client request that triggers the failover
+				assert(peerExpiry > 0);
+				if (get_current_millis() >= peerExpiry)
+				{
+					//  If peer is dead, switch to the active state
+					std::cout << "I: failover successful, ready active\n";
+					state = State::STATE_ACTIVE;
+				}
+				else
+					//  If peer is alive, reject connections
+					exception = true;
+			}
+		}
+		return exception;
+	}
+
+	int64_t get_current_millis()
+	{
+		auto n = std::chrono::system_clock::now();
+		auto m = n.time_since_epoch();
+		auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(m).count();
+		//auto const msecs = diff % 1000;
+		return diff;
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <string>
+#include "fsm.hpp"
+#include <rest_rpc/rpc.hpp>
+
+void test_mili()
+{
+	fsm_t fsm;
+	std::cout << fsm.get_current_millis() << std::endl;
+	std::cout << fsm.get_current_millis() << std::endl;
+	std::cout << fsm.get_current_millis() << std::endl;
+}
+
+void test_fsm(fsm_t fsm)
+{
+	fsm.peerExpiry = fsm.get_current_millis() + 2 * fsm.HEARTBEAT;
+
+	fsm.event = Event::PEER_ACTIVE;
+	bool b = fsm.state_machine();
+
+	//fsm.event = Event::PEER_BACKUP;
+	//b = fsm.state_machine();
+
+	fsm.event = Event::CLIENT_REQUEST;
+	b = fsm.state_machine();
+
+	fsm.event = Event::PEER_BACKUP;
+	b = fsm.state_machine();
+
+	fsm.event = Event::PEER_PASSIVE;
+	b = fsm.state_machine();
+
+	fsm.event = Event::PEER_PRIMARY;
+	b = fsm.state_machine();
+
+	//  We now process events on our two input sockets, and process these
+	//  events one at a time via our finite-state machine. Our "work" for
+	//  a client request is simply to echo it back:
+
+	//  Set timer for next outgoing state message
+	int64_t sendStateAt = fsm.get_current_millis() + fsm.HEARTBEAT;
+	while (true)
+	{
+		//PollItem[] items = 
+		//{
+		//	new PollItem(frontend, ZMQ.Poller.POLLIN),
+		//	new PollItem(statesub, ZMQ.Poller.POLLIN),
+		//};
+		//int timeLeft = (int)((sendStateAt - fsm.get_current_millis()));
+		//if (timeLeft < 0)
+		//	timeLeft = 0;
+		//int rc = ZMQ.poll(items, 2, timeLeft);
+		//if (rc == -1)
+		//	break;              //  Context has been shut down
+
+		//if (items[0].isReadable()) 
+		//{
+		//	//  Have a client request
+		//	ZMsg msg = ZMsg.recvMsg(frontend);
+		//	fsm.event = Event.CLIENT_REQUEST;
+		//	if (fsm.stateMachine() == false)
+		//		//  Answer client by echoing request back
+		//		msg.send(frontend);
+		//	else
+		//		msg.destroy();
+		//}
+		//if (items[1].isReadable()) 
+		//{
+		//	//  Have state from our peer, execute as event
+		//	String message = statesub.recvStr();
+		//	fsm.event = Event.values()[Integer.parseInt(message)];
+		//	if (fsm.stateMachine())
+		//		break;          //  Error, so exit
+		//	fsm.peerExpiry = System.currentTimeMillis() + 2 * HEARTBEAT;
+		//}
+		////  If we timed out, send state to peer
+		//if (System.currentTimeMillis() >= sendStateAt) 
+		//{
+		//	statepub.send(String.valueOf(fsm.state.ordinal()));
+		//	sendStateAt = System.currentTimeMillis() + HEARTBEAT;
+		//}
+	}
+
+	std::cout << "W: interrupted\n";
+
+	//  Shutdown sockets and context
+	//ctx.destroy();
+}
+
+namespace client
+{
+	TIMAX_DEFINE_PROTOCOL(controller, int(int));
+}
+
+int main()
+{
+	std::string str;
+	std::cin >> str;
+
+	timax::log::get().init("rest_rpc_server.lg");
+	using server_t = timax::rpc::server<timax::rpc::msgpack_codec>;
+	std::unique_ptr<server_t> sp = nullptr;
+
+	timax::log::get().init("rest_rpc_client.lg");
+	using async_client = timax::rpc::async_client<timax::rpc::msgpack_codec>;
+	async_client sub_client;
+	boost::asio::ip::tcp::endpoint pub_endpoint;
+	boost::asio::ip::tcp::endpoint sub_endpoint;
+
+	//using async_client = timax::rpc::async_client<timax::rpc::msgpack_codec>;
+	async_client pub_client;
+
+	fsm_t fsm;
+	if (str == "-p")
+	{
+		std::cout<<"I: Primary active, waiting for backup (passive)\n";
+		sp = std::make_unique<server_t>(5001, std::thread::hardware_concurrency());
+		pub_endpoint = timax::rpc::get_tcp_endpoint("127.0.0.1", 5002);
+		sub_endpoint = timax::rpc::get_tcp_endpoint("127.0.0.1", 5001);
+		fsm.state = State::STATE_PRIMARY;
+		std::cout << "I: current state STATE_PRIMARY\n";
+	}
+	else if (str == "-b")
+	{
+		std::cout << "I: Backup passive, waiting for primary (active)\n";
+		sp = std::make_unique<server_t>(5002, std::thread::hardware_concurrency());
+		pub_endpoint = timax::rpc::get_tcp_endpoint("127.0.0.1", 5001);
+		sub_endpoint = timax::rpc::get_tcp_endpoint("127.0.0.1", 5002);
+		fsm.state = State::STATE_BACKUP;
+		std::cout << "I: current state STATE_BACKUP\n";
+	}
+	else 
+	{
+		std::cout << "Usage: bstar { -p | -b }\n";
+		return -1;
+	}
+
+	sp->register_handler("controller", [](int i) { return i; }, [&sp](auto conn, int r) { sp->pub("controller", r); });
+	
+	sp->start();
+	int64_t send_state_at = fsm.get_current_millis() + fsm.HEARTBEAT;
+	std::thread thd([&pub_client, &pub_endpoint, &fsm,&send_state_at]
+	{
+		while (true)
+		{
+			try
+			{
+				send_state_at = fsm.get_current_millis() + fsm.HEARTBEAT;
+				auto task = pub_client.call(pub_endpoint, client::controller, fsm.state);
+				task.wait();
+				//auto r = task.get();
+			}
+			catch (timax::rpc::exception const& e)
+			{
+				std::cout << e.get_error_message() << std::endl;
+			}
+
+			this_thread::sleep_for(std::chrono::milliseconds(1000));
+		}
+	});
+	
+	sub_client.sub(sub_endpoint, client::controller, [&fsm, &send_state_at](auto r)
+	{
+		int timeLeft = (int)((send_state_at - fsm.get_current_millis()));
+		if (timeLeft < 0)
+			timeLeft = 0;
+		std::cout << "timeLeft: "<<timeLeft << std::endl;
+		fsm.event = (Event)r;
+
+		if (fsm.state_machine())
+			throw timax::rpc::exception();          //  Error, so exit
+
+		fsm.peerExpiry = fsm.get_current_millis() + 2 * fsm.HEARTBEAT;
+	},
+		[](auto const& error)
+	{
+		std::cout << error.get_error_message() << std::endl;
+	});
+
+
+	std::cin >> str;
+}
